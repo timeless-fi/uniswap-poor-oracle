eeafa77a028476aceace5efc7c837efa823c1bfe
ZeframLou <zeframlou@gmail.com>
2022-12-26 19:26:01 -0800

    feat: initial implementation

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,22 +2,30 @@
 pragma solidity ^0.8.13;
 
 import {CREATE3Script} from "./base/CREATE3Script.sol";
-import {Contract} from "../src/Contract.sol";
+import {UniswapPoorOracle} from "../src/UniswapPoorOracle.sol";
 
 contract DeployScript is CREATE3Script {
     constructor() CREATE3Script(vm.envString("VERSION")) {}
 
-    function run() external returns (Contract c) {
+    function run() external returns (UniswapPoorOracle oracle) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        uint256 param = 123;
+        uint256 inRangeThreshold = vm.envUint("IN_RANGE_THRESHOLD");
+        require(inRangeThreshold > 0 && inRangeThreshold < 1e18, "Invalid IN_RANGE_THRESHOLD value");
+        uint256 recordingMinLength = vm.envUint("RECORDING_MIN_LENGTH");
+        require(recordingMinLength >= 30 minutes, "Invalid RECORDING_MIN_LENGTH value");
+        uint256 recordingMaxLength = vm.envUint("RECORDING_MAX_LENGTH");
+        require(recordingMinLength <= 1 days, "Invalid RECORDING_MAX_LENGTH value");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        c = Contract(
+        oracle = UniswapPoorOracle(
             create3.deploy(
-                getCreate3ContractSalt("Contract"),
-                bytes.concat(type(Contract).creationCode, abi.encode(param))
+                getCreate3ContractSalt("UniswapPoorOracle"),
+                bytes.concat(
+                    type(UniswapPoorOracle).creationCode,
+                    abi.encode(inRangeThreshold, recordingMinLength, recordingMaxLength)
+                )
             )
         );
 
